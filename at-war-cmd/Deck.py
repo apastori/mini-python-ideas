685a29e29d4b21703fcd430353de9ee472ae8083
apastori <alfonso.pastorimata@gmail.com>
2024-10-04 21:33:40 -0300

    FixesAtWarAndBlackjack

--- at-war-cmd/Deck.py
+++ at-war-cmd/Deck.py
@@ -6,17 +6,10 @@ import random
 class Deck:
     def __init__(self):
         self.all_cards = []
-        self.deck = []
         for suit in suits:
             for rank in ranks:
                 new_card = Card(suit, rank)
                 self.all_cards.append(new_card)
-                self.deck.append(new_card)
-    def __str__(self):
-        deck_str = ''
-        for card in self.all_cards:
-            deck_str += '\n '+ card.__str__() # add each Card object's print string
-        return 'The deck has:' + deck_str
     def shuffle(self):
         random.shuffle(self.all_cards)
     
--- blackjack-cmd/Deck.py
+++ blackjack-cmd/Deck.py
@@ -6,10 +6,17 @@ import random
 class Deck:
     def __init__(self):
         self.all_cards = []
+        self.deck = []
         for suit in suits:
             for rank in ranks:
                 new_card = Card(suit, rank)
                 self.all_cards.append(new_card)
+                self.deck.append(new_card)
+    def __str__(self):
+        deck_str = ''
+        for card in self.deck:
+            deck_str += '\n '+ card.__str__() # add each Card object's print string
+        return 'The deck has:' + deck_str            
 
     def shuffle(self):
         random.shuffle(self.all_cards)
